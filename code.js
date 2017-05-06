12b951be7748df3226cef19c41a8099886d255a2
Edxael <zerphyr.r@gmail.com>
2017-05-05 22:32:07 -0600

    Update on the practice file

--- code.js
+++ code.js
@@ -1,1 +1,9 @@
-console.log("hello people");
+console.log("hello 7");
+
+var xhr = new XMLHttpRequest();
+xhr.open("GET", "https://www.codecademy.com/", false );
+// xhr.open("GET", "https://www.amazon.com/", false );
+
+xhr.send();
+console.log(xhr.status);
+console.log(xhr.statusText);
